a8fb9a10d60f8f015e2f49b9600ad52237750661
MJSantos1s <MJSantos1s@capetigers.com>
2026-01-22 09:52:52 -0600

    The choice

--- js/tny_timer.js
+++ js/tny_timer.js
@@ -11,14 +11,23 @@
 */
 
 function showClock() {
-   var thisDay = Date("May 19, 2018 9:31:27 a.m.")
-   var localDate 
+   var thisDay = Date("May 19, 2018 9:31:27 a.m.");
+   var localDate = thisday.toLocaleDateString();
+   var localTime = thisDay.toLocaleTimeString();
 }
 
 function nextJuly4(currentDate) {
    var cYear = currentDate.getFullYear();
    var jDate = new Date("July 4, 2018");
+   var j4Date = nextJuly4(thisDay);
    jDate.setFullYear(cYear);
    if ((jDate - currentDate) < 0) jDate.setFullYear(cYear + 1);
    return jDate;
+   j4Date = setHours(21);
 }
+
+/*Still needs to be edited for number 5f*/
+document.getElementById("days").textContent = Math.floor(daysLeft);
+document.getElementById("hrs").textContent = Math.floor(hrsLeft);
+document.getElementById("mins").textContent = Math.floor(minsLeft);
+document.getElementById("secs").textContent = Math.floor(secsLeft);
